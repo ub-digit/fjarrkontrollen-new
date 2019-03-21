df01993c84c1466d4c620751fd7c9dac62c15146
Per Larsson <per.larsson@ub.gu.se>
2019-03-21 14:05:41 +0100

    Capistrano and deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,16 @@
 lock '3.4.1'
 
 # Set the application name
-set :application, 'fjarrkontrollen-server'
+set :application, 'fjarrkontrollen'
 
 # Set the repository link
-set :repo_url, 'https://github.com/ub-digit/fjarrkontrollen-server.git'
+set :repo_url, 'https://github.com/ub-digit/fjarrkontrollen.git'
 
 # Set tmp directory on remote host - Default value: '/tmp , which often will not allow files to be executed
 set :tmp_dir, '/home/apps/tmp'
 
 # Copy originals into /{app}/shared/config from respective sample file
-set :linked_files, %w{config/database.yml config/initializers/illbackend_cfg.rb config/initializers/secret_token.rb db/seeds/locations.rb db/seeds/testdata.rb db/seeds/users.rb}
+set :linked_files, %w{config/database.yml config/initializers/illbackend_cfg.rb config/initializers/secret_token.rb}
 
 set :rvm_ruby_version, '2.3.1'      # Defaults to: 'default'
 
@@ -22,6 +22,10 @@ def deploy_config
   return @config[stage.to_s]
 end
 
-server deploy_config['host'], user: deploy_config['user'], roles: deploy_config['roles']
+server deploy_config['host'], port: deploy_config['port'], user: deploy_config['user'], roles: ['app', 'db', 'web']
 
 set :deploy_to, deploy_config['path']
+
+set :default_env, {
+    "PATH" => deploy_config['nvm_path'] + ":$PATH"
+}
