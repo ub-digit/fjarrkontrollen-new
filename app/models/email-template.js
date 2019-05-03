82731cd8fdcfb5ff7b4a974970595d1e24dd51fd
Per Larsson <per.larsson@ub.gu.se>
2019-05-03 13:45:28 +0200

    Add email template label when creating an email note

--- app/models/email-template.js
+++ app/models/email-template.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  label: DS.attr('string'),
   subjectSv: DS.attr('string'),
   subjectEn: DS.attr('string'),
   bodySv: DS.attr('string'),
--- app/models/note.js
+++ app/models/note.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   //userId: DS.belongsTo('user'),
   orderId: DS.attr('string'),
   userId: DS.attr('string'),
+  emailTemplateLabel: DS.attr('string'),
   subject: DS.attr('string'),
   message: DS.attr('string'),
   isEmail: DS.attr('boolean'),
