d38f55c64c497a4367d567e77c6d74a5631a71b6
Johan <johan.larsson@ub.gu.se>
2019-10-31 14:23:35 +0100

    added liquid-fire animation

--- app/transitions.js
+++ app/transitions.js
@@ -5,12 +5,11 @@ export default function() {
     this.use('toLeft', { duration: 100, easing: 'easeInOut' }),
     this.reverse('toRight', { duration: 100, easing: 'easeInOut' })
   );
-  /*
+  
   this.transition(
-    this.fromRoute('login'),
-    this.toRoute('admin.index'),
-    this.use('toUp'),
-    this.reverse('toDown')
+    this.toRoute('admin.user'),
+    this.use('toLeft', { duration: 100, easing: 'easeInOut' }),
+    this.reverse('toRight', { duration: 100, easing: 'easeInOut' })
   );
-  */
+
 }
